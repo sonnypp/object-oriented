934092034b03a068df37fd94afd9cb4d61b95b1d
sonnypp <384282882@qq.com>
2016-02-26 11:15:42 +0800

    Calculator

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 #include<string>
-#include "Class.h"
+#include "calculator.h"
 using namespace std;
+
 
 int main()
 {
-	Scan a;  //定义一个Scan类对象 a . 
-	Print b; //定义一个Print类对象 b . 
-	string input;  //声明字符串 
-	cin>>input;  //输入字符串 
-	a.ToStringQueue(input);  //对字符串进行处理 并且存在queue的队列中 
-	b.PirntStringQueue();   //打印队列 
-	return 0;
-} 
+    Scan a; //创建一个Scan类的对象 a； 
+    Print b; //创建一个Print类的对象 b； 
+    string input; //声明字符串 ； 
+    cin >> input; // 输入字符串 ； 
+    a.ToStringQueue(input);  //处理字符串； 
+    b.PrintStringQueue(); //输出队列； 
+    return 0;
+}
